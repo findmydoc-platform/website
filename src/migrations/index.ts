3ff7a7df9cbabb15b46dec52e261fe4cbff85869
Sebastian S Schütze <sebastian.s.schuetze@deutschebahn.com>
2025-03-15 21:35:12 +0100

    style: format code for improved readability and consistency

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20250224_215508 from './20250224_215508';
+import * as migration_20250224_215508 from './20250224_215508'
 
 export const migrations = [
   {
     up: migration_20250224_215508.up,
     down: migration_20250224_215508.down,
-    name: '20250224_215508'
+    name: '20250224_215508',
   },
-];
+]

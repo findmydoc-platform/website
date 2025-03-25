342f5a0c9590bedecaa1cb5e704e88dfba26a327
Sebastian S Schütze <sebastian.s.schuetze@deutschebahn.com>
2025-03-25 12:38:28 +0100

    feat: add S3 storage configuration and update dependencies

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -290,6 +290,7 @@ export interface Media {
     };
     [k: string]: unknown;
   } | null;
+  prefix?: string | null;
   updatedAt: string;
   createdAt: string;
   url?: string | null;
@@ -1398,6 +1399,7 @@ export interface PostsSelect<T extends boolean = true> {
 export interface MediaSelect<T extends boolean = true> {
   alt?: T;
   caption?: T;
+  prefix?: T;
   updatedAt?: T;
   createdAt?: T;
   url?: T;

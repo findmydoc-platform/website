23297a70f3b8fb15df47457078400fc0c3fbf520
Tolga Cebi <tolgacebii@gmail.com>
2025-03-28 15:29:35 +0100

    feat: add migration script

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20250224_215508 from './20250224_215508';
 import * as migration_20250322_221350 from './20250322_221350';
+import * as migration_20250328_142802 from './20250328_142802';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20250322_221350.up,
     down: migration_20250322_221350.down,
-    name: '20250322_221350'
+    name: '20250322_221350',
+  },
+  {
+    up: migration_20250328_142802.up,
+    down: migration_20250328_142802.down,
+    name: '20250328_142802'
   },
 ];

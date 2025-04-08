78f4470c3192c1634ec2bd33e6ba7e3a9d6a4050
Sebastian S Schütze <sebastian.s.schuetze@deutschebahn.com>
2025-04-08 23:32:58 +0200

    fix: restore S3ClientUploadHandler import and remove unused prefix properties in Media interfaces

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -290,7 +290,6 @@ export interface Media {
     };
     [k: string]: unknown;
   } | null;
-  prefix?: string | null;
   updatedAt: string;
   createdAt: string;
   url?: string | null;
@@ -1399,7 +1398,6 @@ export interface PostsSelect<T extends boolean = true> {
 export interface MediaSelect<T extends boolean = true> {
   alt?: T;
   caption?: T;
-  prefix?: T;
   updatedAt?: T;
   createdAt?: T;
   url?: T;

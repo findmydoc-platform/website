4c138d94ef328870a3b44d18ec0aeeae54222102
Sebastian S Schütze <sebastian.s.schuetze@deutschebahn.com>
2025-05-10 21:50:15 +0200

    feat: Update migration import to include migration for 20250510_193909

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20250509_184036 from './20250509_184036';
+import * as migration_20250510_193909 from './20250510_193909';
 
 export const migrations = [
   {
-    up: migration_20250509_184036.up,
-    down: migration_20250509_184036.down,
-    name: '20250509_184036'
+    up: migration_20250510_193909.up,
+    down: migration_20250510_193909.down,
+    name: '20250510_193909'
   },
 ];

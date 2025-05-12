a36d0eb0a377d43f1ec75514fb8580721832782e
Sebastian Schütze <sebastian.schuetze@razorspoint.com>
2025-05-12 23:53:19 +0200

    feat: Add doctor specialties and update related interfaces (#173)

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20250511_204232 from './20250511_204232';
+import * as migration_20250512_203445 from './20250512_203445';
 
 export const migrations = [
   {
-    up: migration_20250511_204232.up,
-    down: migration_20250511_204232.down,
-    name: '20250511_204232'
+    up: migration_20250512_203445.up,
+    down: migration_20250512_203445.down,
+    name: '20250512_203445'
   },
 ];

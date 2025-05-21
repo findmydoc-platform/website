5bc0d96adb25caeaa02e1c0b26cc0c14ad59a079
Sebastian Schütze <sebastian.s.schuetze@deutschebahn.com>
2025-05-21 16:33:42 +0200

    fix: using s3 storage plugin causes to have mismatch between types and db schema

--- src/migrations/20250521_143232.ts
+++ src/migrations/20250521_143232.ts
@@ -379,6 +379,7 @@ export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   	"id" serial PRIMARY KEY NOT NULL,
   	"alt" varchar,
   	"caption" jsonb,
+  	"prefix" varchar,
   	"updated_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
   	"created_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
   	"url" varchar,
--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,15 +1,9 @@
-import * as migration_20250515_084912 from './20250515_084912';
-import * as migration_20250521_142706 from './20250521_142706';
+import * as migration_20250521_143232 from './20250521_143232';
 
 export const migrations = [
   {
-    up: migration_20250515_084912.up,
-    down: migration_20250515_084912.down,
-    name: '20250515_084912',
-  },
-  {
-    up: migration_20250521_142706.up,
-    down: migration_20250521_142706.down,
-    name: '20250521_142706'
+    up: migration_20250521_143232.up,
+    down: migration_20250521_143232.down,
+    name: '20250521_143232'
   },
 ];

159b7c3348820e23352b77259d0fa08f40138513
Sebastian Schütze <sebastian.s.schuetze@deutschebahn.com>
2025-05-21 16:31:44 +0200

    fix: using s3 storage plugin causes to have mismatch between types and db schema

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20250515_084912 from './20250515_084912';
+import * as migration_20250521_142706 from './20250521_142706';
 
 export const migrations = [
   {
     up: migration_20250515_084912.up,
     down: migration_20250515_084912.down,
-    name: '20250515_084912'
+    name: '20250515_084912',
+  },
+  {
+    up: migration_20250521_142706.up,
+    down: migration_20250521_142706.down,
+    name: '20250521_142706'
   },
 ];

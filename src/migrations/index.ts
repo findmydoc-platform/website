3ae1ff250564cad0a1cf10afb3072eba09f1ecb3
Sebastian Schütze <sebastian.schuetze@razorspoint.com>
2025-08-21 22:13:03 +0300

    feat(migrations): add new UI component tables and enums for pages

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20250731_133127 from './20250731_133127';
 import * as migration_20250808_052756_soft_delete_enablement from './20250808_052756_soft_delete_enablement';
+import * as migration_20250821_191240_ui_components from './20250821_191240_ui_components';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20250808_052756_soft_delete_enablement.up,
     down: migration_20250808_052756_soft_delete_enablement.down,
-    name: '20250808_052756_soft_delete_enablement'
+    name: '20250808_052756_soft_delete_enablement',
+  },
+  {
+    up: migration_20250821_191240_ui_components.up,
+    down: migration_20250821_191240_ui_components.down,
+    name: '20250821_191240_ui_components'
   },
 ];

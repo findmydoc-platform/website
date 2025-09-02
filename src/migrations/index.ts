fa6e0ac7a2fc88fa2b18a2db7a806d7d5cd5acbf
Sebastian Schütze <sebastian.schuetze@razorspoint.com>
2025-09-02 13:21:17 +0200

    Add migration to add temporary password columns to basic_users and patients tables
    - Created a new migration file 20250902_102445_tmp_password_basicusers.ts
    - Added 'temporary_password' column to 'basic_users' table
    - Added 'initial_password' column to 'patients' table
    - Updated migrations index to include the new migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,6 +1,7 @@
-import * as migration_20250731_133127 from './20250731_133127'
-import * as migration_20250808_052756_soft_delete_enablement from './20250808_052756_soft_delete_enablement'
-import * as migration_20250821_191240_ui_components from './20250821_191240_ui_components'
+import * as migration_20250731_133127 from './20250731_133127';
+import * as migration_20250808_052756_soft_delete_enablement from './20250808_052756_soft_delete_enablement';
+import * as migration_20250821_191240_ui_components from './20250821_191240_ui_components';
+import * as migration_20250902_102445_tmp_password_basicusers from './20250902_102445_tmp_password_basicusers';
 
 export const migrations = [
   {
@@ -18,4 +19,9 @@ export const migrations = [
     down: migration_20250821_191240_ui_components.down,
     name: '20250821_191240_ui_components',
   },
-]
+  {
+    up: migration_20250902_102445_tmp_password_basicusers.up,
+    down: migration_20250902_102445_tmp_password_basicusers.down,
+    name: '20250902_102445_tmp_password_basicusers'
+  },
+];

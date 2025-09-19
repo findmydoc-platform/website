dd34ace9c00211fc9d3d8fb5eb788cccdf3b0703
Sebastian Schütze <sebastian.schuetze@razorspoint.com>
2025-09-19 23:22:10 +0200

    Remove user profile consolidation and exports adjustments migration
    - Deleted the migration file `20250918_204219_user_profile_consolidation_and_exports_adjustments.ts.ts` which included changes to user profiles and exports.
    - Updated the migrations index to remove references to the deleted migration.

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,8 +1,7 @@
-import * as migration_20250731_133127 from './20250731_133127';
-import * as migration_20250808_052756_soft_delete_enablement from './20250808_052756_soft_delete_enablement';
-import * as migration_20250821_191240_ui_components from './20250821_191240_ui_components';
-import * as migration_20250918_204219_user_profile_consolidation_and_exports_adjustments.ts from './20250918_204219_user_profile_consolidation_and_exports_adjustments.ts';
-import * as migration_20250918_205529_add_clinic_applications from './20250918_205529_add_clinic_applications';
+import * as migration_20250731_133127 from './20250731_133127'
+import * as migration_20250808_052756_soft_delete_enablement from './20250808_052756_soft_delete_enablement'
+import * as migration_20250821_191240_ui_components from './20250821_191240_ui_components'
+import * as migration_20250918_205529_add_clinic_applications from './20250918_205529_add_clinic_applications'
 
 export const migrations = [
   {
@@ -21,13 +20,8 @@ export const migrations = [
     name: '20250821_191240_ui_components',
   },
   {
-    up: migration_20250918_204219_user_profile_consolidation_and_exports_adjustments.ts.up,
-    down: migration_20250918_204219_user_profile_consolidation_and_exports_adjustments.ts.down,
-    name: '20250918_204219_user_profile_consolidation_and_exports_adjustments.ts',
-  },
-  {
     up: migration_20250918_205529_add_clinic_applications.up,
     down: migration_20250918_205529_add_clinic_applications.down,
-    name: '20250918_205529_add_clinic_applications'
+    name: '20250918_205529_add_clinic_applications',
   },
-];
+]

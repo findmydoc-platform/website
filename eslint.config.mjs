fcef1700f6432f50ab30c6b9006b2129d17b8b80
Sebastian Schütze <sebastian.schuetze@razorspoint.com>
2025-11-15 00:23:25 +0100

    feat(ui): apply tailwind grid rules and add tailwind eslint plugin
    - Update max-width for RichText captions and related posts section
    - Improve layout responsiveness for better user experience

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
 import { FlatCompat } from '@eslint/eslintrc'
+import tailwind from 'eslint-plugin-tailwindcss'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
@@ -12,6 +13,9 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
   {
+    plugins: {
+      tailwindcss: tailwind,
+    },
     rules: {
       '@typescript-eslint/ban-ts-comment': 'warn',
       '@typescript-eslint/no-empty-object-type': 'warn',
@@ -28,6 +32,14 @@ const eslintConfig = [
           caughtErrorsIgnorePattern: '^(_|ignore)',
         },
       ],
+      'tailwindcss/classnames-order': 'warn',
+      'tailwindcss/no-custom-classname': 'off',
+    },
+    settings: {
+      tailwindcss: {
+        callees: ['cn', 'clsx', 'classnames'],
+        config: 'tailwind.config.mjs',
+      },
     },
   },
   {

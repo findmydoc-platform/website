6e2eabc8670bad06d19224fc5e4e1b130992f888
Sebastian Schütze <sebastian.schuetze@razorspoint.com>
2025-12-31 00:07:41 +0100

    ci: add Playwright and Storybook tests to CI workflow (#530)

---
+++ .github/coverage/vitest.thresholds.storybook.js
@@ -0,0 +1,12 @@
+export default {
+  test: {
+    coverage: {
+      thresholds: {
+        statements: 10,
+        branches: 10,
+        functions: 10,
+        lines: 10,
+      },
+    },
+  },
+}

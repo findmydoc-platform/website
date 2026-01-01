70f20857e2cdc7201f413ffe8f71feb71d950984
Sebastian Schütze <sebastian.schuetze@razorspoint.com>
2026-01-02 00:18:54 +0100

    refactor: remove hero components and related code (#536)

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,4 +1,5 @@
-import * as migration_20251124_113216 from './20251124_113216'
+import * as migration_20251124_113216 from './20251124_113216';
+import * as migration_20260101_230558_remove_pages_hero from './20260101_230558_remove_pages_hero';
 
 export const migrations = [
   {
@@ -6,4 +7,9 @@ export const migrations = [
     down: migration_20251124_113216.down,
     name: '20251124_113216',
   },
-]
+  {
+    up: migration_20260101_230558_remove_pages_hero.up,
+    down: migration_20260101_230558_remove_pages_hero.down,
+    name: '20260101_230558_remove_pages_hero'
+  },
+];

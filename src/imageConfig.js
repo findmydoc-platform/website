e40a54c44809043376aef134ee5608d5f070e051
Sebastian Schütze <sebastian.schuetze@razorspoint.com>
2026-01-07 21:24:09 +0100

    fix(imageConfig): refine local patterns for image configuration

--- src/imageConfig.js
+++ src/imageConfig.js
@@ -27,13 +27,11 @@ export const IMAGE_QUALITIES = [DEFAULT_IMAGE_QUALITY, 75]
 
 export const STORYBOOK_IMAGE_LOCAL_PATTERN = {
   pathname: '/src/stories/assets/**',
-  search: '?*',
 }
 
 export const IMAGE_LOCAL_PATTERNS = [
   {
-    pathname: '/**',
-    search: '?*',
+    pathname: '/images/**',
   },
   STORYBOOK_IMAGE_LOCAL_PATTERN,
 ]

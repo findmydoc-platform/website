238a4d5435aff5056074492a4a07b60a7aa13d83
Sebastian Schütze <sebastian.schuetze@razorspoint.com>
2026-01-20 21:26:33 +0100

    feat(migrations): add payload_mcp_api_keys table and update related tables
    - Create new table "payload_mcp_api_keys" with various permissions fields.
    - Add foreign key constraints linking "payload_mcp_api_keys" to "basic_users".
    - Update "payload_locked_documents_rels" and "payload_preferences_rels" to include foreign key references to "payload_mcp_api_keys".
    - Create necessary indexes for performance optimization.
    - Implement down migration to cleanly remove changes if needed.

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,5 @@
 import * as migration_20260112_225205 from './20260112_225205';
-import * as migration_20260120_143832_payload_mcp from './20260120_143832_payload_mcp';
+import * as migration_20260120_202321_payload_mcp from './20260120_202321_payload_mcp';
 
 export const migrations = [
   {
@@ -8,8 +8,8 @@ export const migrations = [
     name: '20260112_225205',
   },
   {
-    up: migration_20260120_143832_payload_mcp.up,
-    down: migration_20260120_143832_payload_mcp.down,
-    name: '20260120_143832_payload_mcp'
+    up: migration_20260120_202321_payload_mcp.up,
+    down: migration_20260120_202321_payload_mcp.down,
+    name: '20260120_202321_payload_mcp'
   },
 ];

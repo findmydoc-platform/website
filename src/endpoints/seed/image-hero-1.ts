a9f0f168fb6de5f2ece93fc3a333fcd7657c7a08
Sebastian Schütze <sebastian.schuetze@razorspoint.com>
2026-01-25 21:54:52 +0100

    chore: Remove seed data for posts and reviews, including related helper functions and data structures.

--- src/endpoints/seed/image-hero-1.ts
+++
@@ -1,5 +0,0 @@
-import type { PlatformContentMedia } from '@/payload-types'
-
-export const imageHero1: Omit<PlatformContentMedia, 'createdAt' | 'id' | 'updatedAt' | 'createdBy' | 'storagePath'> = {
-  alt: 'Straight metallic shapes with a blue gradient',
-}

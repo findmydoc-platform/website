489aadaf4545e71477c328fd88be2ac166c9e597
Sebastian Schütze <sebastian.schuetze@razorspoint.com>
2026-01-26 22:52:45 +0100

    fix: update import statements and fix array syntax in migrations index

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,8 +1,8 @@
-import * as migration_20260112_225205 from './20260112_225205';
-import * as migration_20260120_202321_payload_mcp from './20260120_202321_payload_mcp';
-import * as migration_20260121_075315_add_imports_collection from './20260121_075315_add_imports_collection';
-import * as migration_20260123_101500_user_profile_media_drop_alt_caption from './20260123_101500_user_profile_media_drop_alt_caption';
-import * as migration_20260126_144212_import_plugin from './20260126_144212_import_plugin';
+import * as migration_20260112_225205 from './20260112_225205'
+import * as migration_20260120_202321_payload_mcp from './20260120_202321_payload_mcp'
+import * as migration_20260121_075315_add_imports_collection from './20260121_075315_add_imports_collection'
+import * as migration_20260123_101500_user_profile_media_drop_alt_caption from './20260123_101500_user_profile_media_drop_alt_caption'
+import * as migration_20260126_144212_import_plugin from './20260126_144212_import_plugin'
 
 export const migrations = [
   {
@@ -28,6 +28,6 @@ export const migrations = [
   {
     up: migration_20260126_144212_import_plugin.up,
     down: migration_20260126_144212_import_plugin.down,
-    name: '20260126_144212_import_plugin'
+    name: '20260126_144212_import_plugin',
   },
-];
+]

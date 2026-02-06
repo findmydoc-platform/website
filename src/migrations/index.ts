3b5b4195572950e57d1ed45c1cca052d0500952e
Sebastian Schütze <sebastian.schuetze@razorspoint.com>
2026-02-06 23:28:07 +0100

    Refactor migration imports for consistency and add missing comma in migrations array

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,10 +1,10 @@
-import * as migration_20260112_225205 from './20260112_225205';
-import * as migration_20260120_202321_payload_mcp from './20260120_202321_payload_mcp';
-import * as migration_20260121_075315_add_imports_collection from './20260121_075315_add_imports_collection';
-import * as migration_20260123_101500_user_profile_media_drop_alt_caption from './20260123_101500_user_profile_media_drop_alt_caption';
-import * as migration_20260126_144212_import_plugin from './20260126_144212_import_plugin';
-import * as migration_20260206_103034_compliance_blog_schema from './20260206_103034_compliance_blog_schema';
-import * as migration_20260206_201356_autho_basic_change from './20260206_201356_autho_basic_change';
+import * as migration_20260112_225205 from './20260112_225205'
+import * as migration_20260120_202321_payload_mcp from './20260120_202321_payload_mcp'
+import * as migration_20260121_075315_add_imports_collection from './20260121_075315_add_imports_collection'
+import * as migration_20260123_101500_user_profile_media_drop_alt_caption from './20260123_101500_user_profile_media_drop_alt_caption'
+import * as migration_20260126_144212_import_plugin from './20260126_144212_import_plugin'
+import * as migration_20260206_103034_compliance_blog_schema from './20260206_103034_compliance_blog_schema'
+import * as migration_20260206_201356_autho_basic_change from './20260206_201356_autho_basic_change'
 
 export const migrations = [
   {
@@ -40,6 +40,6 @@ export const migrations = [
   {
     up: migration_20260206_201356_autho_basic_change.up,
     down: migration_20260206_201356_autho_basic_change.down,
-    name: '20260206_201356_autho_basic_change'
+    name: '20260206_201356_autho_basic_change',
   },
-];
+]

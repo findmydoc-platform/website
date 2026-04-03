3194f0a1aeef17d03cee91c10f03425f98f08bc0
Sebastian Schütze <sebastian.schuetze@razorspoint.com>
2026-04-03 21:46:44 +0200

    fix(revalidateRedirects): add context check to skip revalidation

--- src/hooks/revalidateRedirects.ts
+++ src/hooks/revalidateRedirects.ts
@@ -2,7 +2,11 @@ import type { CollectionAfterChangeHook } from 'payload'
 
 import { revalidateTag } from 'next/cache'
 
-export const revalidateRedirects: CollectionAfterChangeHook = ({ doc, req: { payload } }) => {
+export const revalidateRedirects: CollectionAfterChangeHook = ({ doc, req: { context, payload } }) => {
+  if (context.disableRevalidate) {
+    return doc
+  }
+
   payload.logger.info(`Revalidating redirects`)
 
   revalidateTag('redirects', { expire: 0 })
